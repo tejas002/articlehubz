ec0dc5171839c7224e3cdadb042d006e55168371
tejas <tejas.nituk@gmail.com>
2020-05-02 00:26:23 +0530

    useMongoClient option removed from connection

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,8 @@ const mongoose  = require("mongoose");
 const Schema = mongoose.Schema;
 
 // establish mongo connection
-mongoose.connect("mongodb://localhost/articlehub", {useMongoClient: true})
+mongoose.connect("mongodb://localhost/articlehub", {useNewUrlParser: true}).then(() => console.log('Connected to DB'))
+.catch(err =>console.dir('Some Error Occured while connecting',err));
 
 
 mongoose.Promise = Promise
